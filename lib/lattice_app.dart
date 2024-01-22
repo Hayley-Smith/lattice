be9769188c44d3f7ef945b9ef116d9de2d8324f7
Hayley Smith <hayley.j.smith@gmail.com>
2024-01-22 17:58:36 -0600

    inital commit

---
+++ lib/lattice_app.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class LatticeApp extends StatefulWidget {
+  const LatticeApp({super.key});
+
+  @override
+  State<LatticeApp> createState() => _LatticeAppState();
+}
+
+class _LatticeAppState extends State<LatticeApp> {
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp();
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+import 'lattice_app.dart';
+
+//Lattice - A repeating arrangement of points
+
+void main() {
+  runApp(
+      const LatticeApp()
+  );
+}
+
